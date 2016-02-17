889011b8270b4a1d923cb912cc64dfb6d829563b
robalford <robertsalford@gmail.com>
2016-02-16 20:28:22 -0800

    added requirements.txt and modified initializedb and __init__.py to use heroku's environment variables for config

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -15,12 +15,15 @@ from .models import (
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    if 'DATABASE_URL' in os.environ:
+        settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
+    secret = os.environ.get('AUTH_SECRET', 'somesecret')
     config = Configurator(
         settings=settings,
-        authentication_policy=AuthTktAuthenticationPolicy('somesecret'),
+        authentication_policy=AuthTktAuthenticationPolicy(secret),
         authorization_policy=ACLAuthorizationPolicy(),
         default_permission='view'
     )
